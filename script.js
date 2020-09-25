3d0baaf726738dddf323b3323466ff055ee92b33
Karol <kargol92@gmail.com>
2020-09-25 11:19:52 +0200

    fixes

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function checkCommand() {
     		"CONFIG   SYS		 46,485	01/01/2020 12:00\n" +
     		"MSCONFIG SYS		    740	01/01/2020 12:00\n" +
     		"	4 File(s)	116,063 Bytes.\n" +
-    		"	3 Dir(s)    262,111,744 Bytes.\n";
+    		"	3 Dir(s)    262,111,744 Bytes free.\n";
     } else if (command == "exit") {
     	window.close();
     } else if (command == "ver") {
